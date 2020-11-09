195f4c21fb0136145690f92ea1c8a170fae46302
Ethan Dunzer <ethandunzer@yahoo.com>
2020-11-09 15:03:36 -0800

    week 8 database files

---
+++ Relational Database Design I/WEEK 8/LAB 8.sql
@@ -0,0 +1,31 @@
+-- LAB 8 ---
+
+-- QUESTION 4
+
+/*
+Explain the difference between scaling up and scaling out.
+*/
+
+-- QUESTION 9
+
+/*
+What are the key assumptions made by the Hadoop Distributed File System appraoch.
+*/
+
+-- QUESTION 14
+
+/*
+How are the value components of a key-value database and a document database different.
+*/
+
+-- QUESTION 19
+
+/*
+Describe and contrast the focus of data mining and predicitve analytics. Give some examples.
+*/
+
+-- QUESTION 20
+
+/*
+How does data mining work? Discuss the different phases in the data minging process.
+*/
